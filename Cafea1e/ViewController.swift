0d632b372f49bc90340f76efe1990b693a4c35fc
Antoine <antoine.galtier@airinov.fr>
2016-06-03 15:36:56 +0200

    Initial Commit

---
+++ Cafea1e/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cafea1e
+//
+//  Created by Antoine GALTIER on 03/06/2016.
+//  Copyright © 2016 Teiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
